b119eb0766ff57877ae13c923d8131f3c721f1f4
sinlov <sinlovgmppt@gmail.com>
2017-08-31 14:40:13 +0800

    add script for develop

---
+++ z-refresh-dependencies-win.bat
@@ -0,0 +1,12 @@
+where git
+where java
+where gradle
+where android
+
+call git status
+call git pull
+call git branch -v
+
+call gradlew.bat clean
+
+call gradlew.bat :app:dependencies --refresh-dependencies
